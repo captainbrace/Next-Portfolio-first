6732eab6ae2126e82a973ae93c4d8a4480c6c872
captainbrace <kwekuandoh10@gmail.com>
2026-06-11 22:32:31 +0000

    fix: added missing root layout file

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'My Portfolio',
+  description: 'Welcome to my professional portfolio website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body>
+        {/* This "children" prop is where your page.js content will be injected */}
+        {children}
+      </body>
+    </html>
+  );
+}
